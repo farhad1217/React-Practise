478ea92f90154e3d037dd59bc5d9998e621a1f17
farhad1217 <gle.farhad@gmail.com>
2026-02-10 01:42:10 +0600

    Multiple Selected added

--- practising react/src/component/accordian/index.jsx
+++ practising react/src/component/accordian/index.jsx
@@ -2,25 +2,37 @@ import { useState } from "react";
 import data from "./data.js";
 
 export default function Accordian() {
-    const [selected, setSelected] = useState("0")
-    function handleClick(clickedID){
-        setSelected(clickedID === selected? null: clickedID)
-    }
+  const [multiSelect, setMultiSelect] = useState(false);
+  const [selected, setSelected] = useState(["1"]);
+  function handleClick(clickedID) {
+    multiSelect
+      ? setSelected((prevSelected) =>
+          prevSelected.includes(clickedID)
+            ? prevSelected.filter((id) => id !== clickedID)
+            : [...prevSelected, clickedID],
+        )
+      : setSelected(selected[0] === clickedID ? [] : [clickedID]);
+  }
+  function toggleSelectioin() {
+    setMultiSelect((prev) => !prev);
+    selected.length > 1 ? setSelected("1") : setSelected(selected);
+  }
   return (
     <>
       {data.map((dataItem) => (
-        <div key={dataItem.id} onClick={()=>handleClick(dataItem.id)}>
+        <div key={dataItem.id} onClick={() => handleClick(dataItem.id)}>
           <div className="question">
             <h2>{dataItem.question}</h2>
             <span>+</span>
           </div>
           <div className="anwser">
-            {selected === dataItem.id? dataItem.answer : null}
+            {selected.includes(dataItem.id) ? dataItem.answer : null}
           </div>
         </div>
       ))}
+      <button onClick={toggleSelectioin}>
+        {multiSelect ? "Disable Multiselect" : "Enable Multiselect"}
+      </button>
     </>
   );
 }
-
-
